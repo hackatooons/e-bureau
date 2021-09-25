ed6c33060b3fc9d4d660f57b00cdaaf49bdd7508
madiha123-design <70858557+madiha123-design@users.noreply.github.com>
2021-09-25 21:25:02 +0530

    updated files

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,7 @@
 import Head from 'next/head'
+import styles from '../asset/index.module.css'
+import Header from '../components/Header/Header'
+import Footer from '../components/Footer/Footer'
 import Form from '../components/Form'
 
 export default function Home({ isConnected }) {
@@ -6,8 +9,278 @@ export default function Home({ isConnected }) {
     <>
       <Head>
         <title>E-bureau</title>
+        <link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/styles/tailwind.css" />
+        <link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css" />
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" rel="stylesheet" />
       </Head>
-      <Form title="General Diary" fields={['Emails', 'Sample', 'Sample']} />
+      <Header />
+      <section className="relative  bg-blueGray-50">
+        <div className="relative pt-16 pb-32 flex content-center items-center justify-center min-h-screen-75">
+          <div className="absolute top-0 w-full h-full bg-center bg-cover" id={styles.banner}>
+            <span id="blackOverlay" className="w-full h-full absolute opacity-75 bg-black"></span>
+          </div>
+          <div className="container relative mx-auto">
+            <div className="items-center flex flex-wrap">
+              <div className="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
+                <div className="pr-12">
+                  <h1 className="text-white font-semibold text-5xl">
+                    Crime Management E- Bureau.
+                  </h1>
+                  <p className="mt-4 text-lg text-blueGray-200">
+                    This is an E- platform used by both public and police for making Crime Management easier.
+                    This includes general diary, crime reports and reporting of missing person.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden h-70-px">
+            <svg className="absolute bottom-0 overflow-hidden" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" version="1.1" viewBox="0 0 2560 100" x="0" y="0">
+              <polygon className="text-blueGray-200 fill-current" points="2560 0 2560 100 0 100"></polygon>
+            </svg>
+          </div>
+        </div>
+      </section>
+
+      <section className="pb-10 bg-blueGray-200 -mt-24">
+        <div className="container mx-auto px-4" />
+        <div className="flex flex-wrap">
+          <div className="lg:pt-12 pt-6 w-full md:w-4/12 px-4 text-center" id={styles.cards}>
+            <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
+              <div className="px-4 py-5 flex-auto">
+                <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-red-400">
+                  <i className="fas fa-award"></i>
+                </div>
+                <h6 className="text-xl font-semibold">General Diary</h6>
+                <p className="mt-2 mb-4 text-blueGray-500">
+                  Divide details about your product or agency work into parts.
+                  A paragraph describing a feature will be enough.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="w-full md:w-4/12 px-4 text-center" id={styles.cards}>
+            <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
+              <div className="px-4 py-5 flex-auto">
+                <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-lightBlue-400">
+                  <i className="fas fa-retweet"></i>
+                </div>
+                <h6 className="text-xl font-semibold">Crime Report</h6>
+                <p className="mt-2 mb-4 text-blueGray-500">
+                  Keep you user engaged by providing meaningful information.
+                  Remember that by this time, the user is curious.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="pt-6 w-full md:w-4/12 px-4 text-center" id={styles.cards}>
+            <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
+              <div className="px-4 py-5 flex-auto">
+                <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-emerald-400">
+                  <i className="fas fa-fingerprint"></i>
+                </div>
+                <h6 className="text-xl font-semibold">Missing Report</h6>
+                <p className="mt-2 mb-4 text-blueGray-500">
+                  Write a few lines about each one.A paragraph describing a
+                  feature will be enough.Keep you user engaged!
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="relative pt-16 bg-blueGray-50">
+        <div className="container mx-auto">
+          <div className="flex flex-wrap items-center">
+            <div className="w-10/12 md:w-6/12 lg:w-4/12 px-12 md:px-4 mr-auto ml-auto -mt-78">
+              <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg bg-pink-500">
+                <img alt="..." src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=700&amp;q=80" className="w-full align-middle rounded-t-lg" />
+                <blockquote className="relative p-8 mb-4">
+                  <svg preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 583 95" className="absolute left-0 w-full block h-95-px -top-94-px">
+                    <polygon points="-30,95 583,95 583,65" className="text-pink-500 fill-current"></polygon>
+                  </svg>
+                  <h4 className="text-xl font-bold text-white">
+                    Great for your awesome project
+                  </h4>
+                  <p className="text-md font-light mt-2 text-white">
+                    Putting together a page has never been easier than matching
+                    together pre-made components.From landing pages presentation
+                    to login areas, you can easily customise and built your pages.
+                  </p>
+                </blockquote>
+              </div>
+            </div>
+            <div className="w-full md:w-6/12 px-4">
+              <div className="flex flex-wrap">
+                <div className="w-full md:w-6/12 px-4">
+                  <div className="relative flex flex-col mt-4">
+                    <div className="px-4 py-5 flex-auto">
+                      <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-white">
+                        <i className="fas fa-sitemap"></i>
+                      </div>
+                      <h6 className="text-xl mb-1 font-semibold">CSS Components</h6>
+                      <p className="mb-4 text-blueGray-500">
+                        Notus JS comes with a huge number of Fully Coded CSS
+                        components.
+                      </p>
+                    </div>
+                  </div>
+                  <div className="relative flex flex-col min-w-0">
+                    <div className="px-4 py-5 flex-auto">
+                      <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-white">
+                        <i className="fas fa-drafting-compass"></i>
+                      </div>
+                      <h6 className="text-xl mb-1 font-semibold">
+                        JavaScript Components
+                      </h6>
+                      <p className="mb-4 text-blueGray-500">
+                        We also feature many dynamic components for React, NextJS,
+                        Vue and Angular.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="w-full md:w-6/12 px-4">
+                  <div className="relative flex flex-col min-w-0 mt-4">
+                    <div className="px-4 py-5 flex-auto">
+                      <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-white">
+                        <i className="fas fa-newspaper"></i>
+                      </div>
+                      <h6 className="text-xl mb-1 font-semibold">Pages</h6>
+                      <p className="mb-4 text-blueGray-500">
+                        This extension also comes with 3 sample pages. They are
+                        fully coded so you can start working instantly.
+                      </p>
+                    </div>
+                  </div>
+                  <div className="relative flex flex-col min-w-0">
+                    <div className="px-4 py-5 flex-auto">
+                      <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-white">
+                        <i className="fas fa-file-alt"></i>
+                      </div>
+                      <h6 className="text-xl mb-1 font-semibold">Documentation</h6>
+                      <p className="mb-4 text-blueGray-500">
+                        Built by developers for developers. You will love how easy
+                        is to to work with Notus JS.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className=" bg-blueGray-200 -mt-24">
+        <div className="container mx-auto px-4">
+          <div className="flex flex-wrap">
+            <div className="lg:pt-12 pt-6 w-full md:w-4/12 px-4 text-center">
+              <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-1 shadow-lg rounded-lg">
+                <div className="px-4 flex-auto">
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-wrap items-center mt-16">
+            <div className="w-full md:w-5/12 px-4 mr-auto ml-auto">
+              <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-white">
+                <i className="fas fa-user-friends text-xl"></i>
+              </div>
+              <h3 className="text-3xl mb-2 font-semibold leading-normal">
+                Working with us is a pleasure
+              </h3>
+              <p className="text-lg font-light leading-relaxed mt-4 mb-4 text-blueGray-600">
+                Don't let your uses guess by attaching tooltips and popoves to
+                any element. Just make sure you enable them first via
+                JavaScript.
+              </p>
+              <p className="text-lg font-light leading-relaxed mt-0 mb-4 text-blueGray-600">
+                The kit comes with three pre-built pages to help you get started
+                faster. You can change the text and images and you're good to
+                go. Just make sure you enable them first via JavaScript.
+              </p>
+              <a href="../index.html" className="font-bold text-blueGray-700 mt-8">Check Notus JS!</a>
+            </div>
+            <div className="w-full md:w-4/12 px-4 mr-auto ml-auto">
+              <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-2 shadow-lg rounded-lg bg-pink-500">
+                <img alt="..." src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1051&amp;q=80" className="w-full align-middle rounded-t-lg" />
+                <blockquote className="relative p-8 mb-4">
+                  <svg preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 583 95" className="absolute left-0 w-full block h-95-px -top-94-px">
+                    <polygon points="-30,95 583,95 583,65" className="text-pink-500 fill-current"></polygon>
+                  </svg>
+
+                  <h4 className="text-xl font-bold text-white">
+                    Top Notch Services
+                  </h4>
+                  <p className="text-md font-light mt-2 text-white">
+                    The Arctic Ocean freezes every winter and much of the
+                    sea-ice then thaws every summer, and that process will
+                    continue whatever happens.
+                  </p>
+                </blockquote>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="relative pt-12 bg-blueGray-50">
+        <div className="items-center flex flex-wrap">
+          <div className="w-full md:w-4/12 ml-auto mr-auto px-4">
+            <img alt="..." className="max-w-full rounded-lg shadow-lg" src="https://images.unsplash.com/photo-1555212697-194d092e3b8f?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=634&amp;q=80" />
+          </div>
+          <div className="w-full md:w-5/12 ml-auto mr-auto px-4">
+            <div className="md:pr-12">
+              <div className="text-pink-600 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-pink-300 mt-8">
+                <i className="fas fa-rocket text-xl"></i>
+              </div>
+              <h3 className="text-3xl font-semibold">A growing company</h3>
+              <p className="mt-4 text-lg leading-relaxed text-blueGray-500">
+                The extension comes with three pre-built pages to help you get
+                started faster. You can change the text and images and you're
+                good to go.
+              </p>
+              <ul className="list-none mt-6">
+                <li className="py-2">
+                  <div className="flex items-center">
+                    <div>
+                      <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-pink-600 bg-pink-200 mr-3"><i className="fas fa-fingerprint"></i></span>
+                    </div>
+                    <div>
+                      <h4 className="text-blueGray-500">
+                        Carefully crafted components
+                      </h4>
+                    </div>
+                  </div>
+                </li>
+                <li className="py-2">
+                  <div className="flex items-center">
+                    <div>
+                      <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-pink-600 bg-pink-200 mr-3"><i className="fab fa-html5"></i></span>
+                    </div>
+                    <div>
+                      <h4 className="text-blueGray-500">Amazing page examples</h4>
+                    </div>
+                  </div>
+                </li>
+                <li className="py-2">
+                  <div className="flex items-center">
+                    <div>
+                      <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-pink-600 bg-pink-200 mr-3"><i className="far fa-paper-plane"></i></span>
+                    </div>
+                    <div>
+                      <h4 className="text-blueGray-500">Dynamic components</h4>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+      <Footer />
     </>
   )
 }
