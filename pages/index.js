ce995914346ddf4e50ad2eb34eae0db0e9c234f5
madiha123-design <70858557+madiha123-design@users.noreply.github.com>
2021-09-25 21:47:17 +0530

    added link to cards

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import styles from '../asset/index.module.css'
 import Header from '../components/Header/Header'
 import Footer from '../components/Footer/Footer'
 import Form from '../components/Form'
+import Link from 'next/link'
 
 export default function Home({ isConnected }) {
   return (
@@ -45,48 +46,54 @@ export default function Home({ isConnected }) {
       <section className="pb-10 bg-blueGray-200 -mt-24">
         <div className="container mx-auto px-4" />
         <div className="flex flex-wrap">
-          <div className="lg:pt-12 pt-6 w-full md:w-4/12 px-4 text-center" id={styles.cards}>
-            <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
-              <div className="px-4 py-5 flex-auto">
-                <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-red-400">
-                  <i className="fas fa-award"></i>
+          <Link href="/" >
+            <div className="lg:pt-12 pt-6 w-full md:w-4/12 px-4 text-center" id={styles.cards}>
+              <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg" id={styles.eachCard}>
+                <div className="px-4 py-5 flex-auto">
+                  <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-red-400">
+                    <i className="fas fa-award"></i>
+                  </div>
+                  <h6 className="text-xl font-semibold">General Diary</h6>
+                  <p className="mt-2 mb-4 text-blueGray-500" >
+                    Divide details about your product or agency work into parts.
+                    A paragraph describing a feature will be enough.
+                  </p>
                 </div>
-                <h6 className="text-xl font-semibold">General Diary</h6>
-                <p className="mt-2 mb-4 text-blueGray-500">
-                  Divide details about your product or agency work into parts.
-                  A paragraph describing a feature will be enough.
-                </p>
               </div>
             </div>
-          </div>
-          <div className="w-full md:w-4/12 px-4 text-center" id={styles.cards}>
-            <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
-              <div className="px-4 py-5 flex-auto">
-                <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-lightBlue-400">
-                  <i className="fas fa-retweet"></i>
+          </Link>
+          <Link href="/">
+            <div className="w-full md:w-4/12 px-4 text-center" id={styles.cards}>
+              <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg" id={styles.eachCard}>
+                <div className="px-4 py-5 flex-auto">
+                  <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-lightBlue-400">
+                    <i className="fas fa-retweet"></i>
+                  </div>
+                  <h6 className="text-xl font-semibold">Crime Report</h6>
+                  <p className="mt-2 mb-4 text-blueGray-500">
+                    Keep you user engaged by providing meaningful information.
+                    Remember that by this time, the user is curious.
+                  </p>
                 </div>
-                <h6 className="text-xl font-semibold">Crime Report</h6>
-                <p className="mt-2 mb-4 text-blueGray-500">
-                  Keep you user engaged by providing meaningful information.
-                  Remember that by this time, the user is curious.
-                </p>
               </div>
             </div>
-          </div>
-          <div className="pt-6 w-full md:w-4/12 px-4 text-center" id={styles.cards}>
-            <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
-              <div className="px-4 py-5 flex-auto">
-                <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-emerald-400">
-                  <i className="fas fa-fingerprint"></i>
+          </Link>
+          <Link href="/">
+            <div className="pt-6 w-full md:w-4/12 px-4 text-center" id={styles.cards}>
+              <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg" id={styles.eachCard}>
+                <div className="px-4 py-5 flex-auto">
+                  <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-emerald-400">
+                    <i className="fas fa-fingerprint"></i>
+                  </div>
+                  <h6 className="text-xl font-semibold">Missing Report</h6>
+                  <p className="mt-2 mb-4 text-blueGray-500">
+                    Write a few lines about each one.A paragraph describing a
+                    feature will be enough.Keep you user engaged!
+                  </p>
                 </div>
-                <h6 className="text-xl font-semibold">Missing Report</h6>
-                <p className="mt-2 mb-4 text-blueGray-500">
-                  Write a few lines about each one.A paragraph describing a
-                  feature will be enough.Keep you user engaged!
-                </p>
               </div>
             </div>
-          </div>
+          </Link>
         </div>
       </section>
 
