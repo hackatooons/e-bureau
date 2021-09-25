4d7fedb3682c6fa4a6a87c80e2605fa8da268b0e
Srijan Kr. Gupta <gupta.srijan94@gmail.com>
2021-09-25 14:17:22 +0530

    Created schema for GD

---
+++ models/Gd.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const gdSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+    maxlength: [40, 'Name cannot be more than 40 characters'],
+  },
+  age: {
+    type: String,
+    require: true,
+    maxlength: [40, 'Name cannot be more than 40 characters'],
+  },
+  picture: {
+    type: ArrayBuffer,
+  },
+  place: {
+    type: String,
+    require: true,
+  },
+  descr: {
+    type: String,
+    maxlength: [200, 'Description cannot be more than 200 characters'],
+  },
+})
+
+module.exports = mongoose.models.Gd || mongoose.model('Gd', gdSchema)
