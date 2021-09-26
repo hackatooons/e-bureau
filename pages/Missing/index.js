d0c901c3fb7071d4f993819af0dde076143e0c25
Srijan Kr. Gupta <gupta.srijan94@gmail.com>
2021-09-26 19:00:30 +0530

    Update links

--- pages/Missing/index.js
+++ pages/Missing/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
+import Link from 'next/link'
 
 const generalDiary = () => {
   return (
