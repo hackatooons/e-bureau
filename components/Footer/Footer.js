ad5c52fee054cd46872b48df38737f5c7e003267
madiha123-design <70858557+madiha123-design@users.noreply.github.com>
2021-09-26 19:49:32 +0530

    added links to footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,18 +1,31 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Footer = () => {
   return (
     <>
-      <footer className="relative bg-gray-300 pt-8 pb-6">
+      <link
+        rel="stylesheet"
+        href="https://demos.creative-tim.com/notus-js/assets/styles/tailwind.css"
+      />
+      <link
+        rel="stylesheet"
+        href="https://demos.creative-tim.com/notus-js/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css"
+      />
+      <link
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css"
+        rel="stylesheet"
+      />
+      <footer className="relative bg-blueGray-200 pt-8 pb-6">
         <div className="container mx-auto px-4">
           <div className="flex flex-wrap text-left lg:text-left">
             <div className="w-full lg:w-6/12 px-4">
-              <h4 className="text-3xl fonat-semibold text-gray-900">
+              <h4 className="text-3xl fonat-semibold text-blueGray-700">
                 Let's us together bring change!
               </h4>
               <h5 className="text-lg mt-0 mb-2 text-blueGray-600">
-                File complain, track using tracking Id. Then sit back and relax,
-                let the police Officers solve the problem.
+                File complain, track using tracking Id.
+                Then sit back and relax, let the police Officers solve the problem.
               </h5>
               <div className="mt-6 lg:mb-0 mb-6">
                 <button
@@ -38,7 +51,7 @@ const Footer = () => {
             <div className="w-full lg:w-6/12 px-4">
               <div className="flex flex-wrap items-top mb-6">
                 <div className="w-full lg:w-4/12 px-4 ml-auto">
-                  <span className="block uppercase text-gray-600 text-sm font-semibold mb-2">
+                  <span className="block uppercase text-blueGray-800 text-sm font-semibold mb-2">
                     Useful Links
                   </span>
                   <ul className="list-unstyled">
@@ -50,34 +63,40 @@ const Footer = () => {
                         About Us
                       </a>
                     </li>
-                    <li>
-                      <a
-                        className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-                        href=""
-                      >
-                        General Diary
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-                        href=""
-                      >
-                        Missing Report
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-                        href=""
-                      >
-                        Crime Report
-                      </a>
-                    </li>
+                    <Link href="/GeneralDiary">
+                      <li>
+                        <a
+                          className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                          href=""
+                        >
+                          General Diary
+                        </a>
+                      </li>
+                    </Link>
+                    <Link href="/Missing">
+                      <li>
+                        <a
+                          className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                          href=""
+                        >
+                          Missing Report
+                        </a>
+                      </li>
+                    </Link>
+                    <Link href="/crime">
+                      <li>
+                        <a
+                          className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                          href=""
+                        >
+                          Crime Report
+                        </a>
+                      </li>
+                    </Link>
                   </ul>
                 </div>
                 <div className="w-full lg:w-4/12 px-4">
-                  <span className="block uppercase text-gray-600 text-sm font-semibold mb-2">
+                  <span className="block uppercase text-blueGray-800 text-sm font-semibold mb-2">
                     Other Links
                   </span>
                   <ul className="list-unstyled">
@@ -121,15 +140,18 @@ const Footer = () => {
           <hr className="my-6 border-blueGray-300" />
           <div className="flex flex-wrap items-center md:justify-between justify-center">
             <div className="w-full md:w-4/12 px-4 mx-auto text-center">
-              <div className="text-sm text-gray-600 font-semibold py-1">
+              <div className="text-sm text-blueGray-500 font-semibold py-1">
                 Copyright © <span id="get-current-year">2021</span>
                 <a
                   href=""
-                  className="text-gray-600 hover:text-gray-800"
+                  className="text-blueGray-500 hover:text-gray-800"
                   target="_blank"
                 />{' '}
                 all rights
-                <a href="" className="text-gray-600 hover:text-blueGray-800">
+                <a
+                  href=""
+                  className="text-blueGray-500 hover:text-blueGray-800"
+                >
                   {' '}
                   reserved
                 </a>
