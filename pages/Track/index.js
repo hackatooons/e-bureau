ec3851450cc46290ae64fc9f0e67bc35844fc922
Srijan Kr. Gupta <gupta.srijan94@gmail.com>
2021-09-26 22:14:36 +0530

    Update files and Fixes

--- pages/Track/index.js
+++ pages/Track/index.js
@@ -1,32 +1,63 @@
 import React from 'react'
 import Footer from '../../components/Footer/Footer'
 import Header from '../../components/Header/Header'
+import styles from './track.module.css'
+import { GrSearchAdvanced } from 'react-icons/gr'
+import { useState } from 'react'
 
 const Page = () => {
+  const [searchInput, setSearchInput] = useState('')
+  const [caseType, setCaseType] = useState('crime')
+  const [result, setResult] = useState({})
+
+  async function getResult() {
+    try {
+      const res = await fetch(`/api/${caseType}/${searchInput}/`)
+      const json = await res.json()
+      setResult(json)
+      console.log(result)
+    } catch (e) {
+      console.error(e)
+    }
+  }
   return (
     <>
       <Header />
-      <div className="p-8">
-        <div className="flex items-center rounded-full shadow-xl">
-          <input
-            className="rounded-l-full w-full py-4 px-6 text-gray-700 leading-tight focus:outline-none"
-            id="search"
-            type="text"
-            placeholder="Search"
-          />
-
-          <div className="p-4">
-            <button className="bg-blue-500 text-white rounded-full p-2 hover:bg-blue-400 focus:outline-none w-12 h-12 flex items-center justify-center">
-              icon
-            </button>
+      <div className="px-20 mt-40">
+        <div className="my-5">
+          <label for="collection" className="text-xl">
+            Select Type
+          </label>
+          <div class="select">
+            <select
+              id="collection"
+              value={caseType}
+              onChange={(e) => setCaseType(e.target.value)}
+              onBlur={(e) => setCaseType(e.target.value)}
+            >
+              <option value="crime">Crime</option>
+              <option value="gd">General Diary</option>
+              <option value="missing">Missing</option>
+            </select>
           </div>
         </div>
+        <div className="shadow-2xl flex">
+          <input
+            className="w-full rounded p-2"
+            type="text"
+            placeholder="Enter unique reference number"
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
+          />
+          <button
+            onClick={(e) => getResult()}
+            className="bg-white w-auto flex justify-end items-center text-blue-500 p-2 hover:text-blue-400"
+          >
+            <GrSearchAdvanced />
+          </button>
+        </div>
+        {JSON.stringify(result, null, '\n')}
       </div>
-      <div
-        className="min-h-screen"
-        style={{ backgroundColor: '#e4e4e7' }}
-      ></div>
-      <Footer />
     </>
   )
 }
