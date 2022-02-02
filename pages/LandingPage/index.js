fca0b3fc51d5047344bc278b42e26c9a93725481
madiha123-design <70858557+madiha123-design@users.noreply.github.com>
2022-02-02 20:00:35 +0530

    resolved issue

--- pages/LandingPage/index.js
+++ pages/LandingPage/index.js
@@ -1,58 +1,54 @@
 import React from "react"
 // import "../../components/Header/Header"
-// import "../LandingPage/landingpg.css"
-import styles from '../../asset/index.module.css'
-// import Image from '../LandingPage/Data_security_24-removebg-preview.png'
- import  Image from "../../asset/Data_security_24-removebg-preview.png"
+import styles from '../../asset/css/index.module.css'
 // import Footer from '../../components/Footer/Footer'
 function page() {
   return (
-    
+
     <div className={styles.outerdiv}>
-      
       <div className={styles.bubbles}>
-      <div className={styles.bubble}></div>
-      <div className={styles.bubble}></div>
-      <div className={styles.bubble}></div>
-      <div className={styles.bubble}></div>
-      <div className={styles.bubble}></div>
-      <div className={styles.bubble}></div>
-      <div className={styles.bubble}></div>
-      <div className={styles.bubble}></div>
-      <div className={styles.bubble}></div>
-      <div className={styles.bubble}></div>
-      <div className={styles.bubble}></div>
-      <div className={styles.bubble}></div>
-      <div className={styles.bubble}></div>
-      <div className={styles.bubble}></div>
-      <div className={styles.bubble}></div>
+        <div className={styles.bubble}></div>
+        <div className={styles.bubble}></div>
+        <div className={styles.bubble}></div>
+        <div className={styles.bubble}></div>
+        <div className={styles.bubble}></div>
+        <div className={styles.bubble}></div>
+        <div className={styles.bubble}></div>
+        <div className={styles.bubble}></div>
+        <div className={styles.bubble}></div>
+        <div className={styles.bubble}></div>
+        <div className={styles.bubble}></div>
+        <div className={styles.bubble}></div>
+        <div className={styles.bubble}></div>
+        <div className={styles.bubble}></div>
+        <div className={styles.bubble}></div>
+      </div>
+      <h6 className={styles.headingsec}>A crime management platform</h6>
+      <h3 className={styles.heading}>WELCOME TO E-bureau</h3>
+
+      <hr className={styles.horiBarFirst} />
+      <button className={styles.circlebtn}></button>
+
+      <hr className={styles.horiBarSecond} />
+
+      <div className={styles.firstDiv}>
+        <p>
+          This is a E-platform which aims to help to make crime management easier.
+          This includes general diary, crime reports and reporting of missing
+          person.
+
+        </p>
+      </div>
+      <button className={styles.firstBtn}>SignUp</button>
+      <button className={styles.secondBtn}>Login</button>
+      <img
+        src="https://www.linkpicture.com/q/Data.png"
+        className={styles.hackerimg}
+        alt=""
+      />
+
+
     </div>
-        <h6 className={styles.headingsec}>A crime management platform</h6>
-        <h3 className={styles.heading}>WELCOME TO E-bureau</h3>
-    
-        <hr className={styles.horiBarFirst} />
-        <button className={styles.circlebtn}></button>
-      
-        <hr className={styles.horiBarSecond} />
-    
-        <div className={styles.firstDiv}>
-          <p>
-            This is a E-platform which aims to help to make crime management easier.
-            This includes general diary, crime reports and reporting of missing
-            person.
-           
-          </p>
-        </div>
-        <button className={styles.firstBtn}>SignUp</button>
-        <button className={styles.secondBtn}>Login</button>
-        <img
-          src={Image}
-          className={styles.hackerimg}
-          alt=""
-        />
-       
-           
-    </div>
-    )
+  )
 }
 export default page;
