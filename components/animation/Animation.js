9439d2ea2a3de9e09d6a59dd21636bf84163abc3
Vishwa Gaurav <vishwagauravin@gmail.com>
2022-03-01 20:13:22 +0530

    moved Animation to separate component

---
+++ components/animation/Animation.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import styles from '../../asset/css/animation.module.css'
+
+export const Animation = () => {
+  return (
+    <div className={styles.bubbles}>
+      <div className={styles.bubble}></div>
+      <div className={styles.bubble}></div>
+      <div className={styles.bubble}></div>
+      <div className={styles.bubble}></div>
+      <div className={styles.bubble}></div>
+      <div className={styles.bubble}></div>
+      <div className={styles.bubble}></div>
+      <div className={styles.bubble}></div>
+      <div className={styles.bubble}></div>
+      <div className={styles.bubble}></div>
+      <div className={styles.bubble}></div>
+      <div className={styles.bubble}></div>
+      <div className={styles.bubble}></div>
+      <div className={styles.bubble}></div>
+      <div className={styles.bubble}></div>
+    </div>
+  )
+}
+
+export default Animation
