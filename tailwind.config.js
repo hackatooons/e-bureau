f02333ab887c121e6232d861992946ab35b431cd
Jaydeep <jaydeep.dey03@gmail.com>
2022-03-07 00:14:28 +0530

    Added Post Mortem page and Add Prisoner Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,7 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      fontFamily:{
-        'Fredoka': ['Fredoka', 'sans-serif'],
-      }
-    },
+    extend: {},
   },
   variants: {
     extend: {},
