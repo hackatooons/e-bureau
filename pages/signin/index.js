2add06f2e5694801305549f264e508090ba68e45
Raghvendra Singh <raghvendrrsingh@gmail.com>
2022-04-13 21:23:41 +0530

    Added formik and Yup for validation before sign-in and sign-up

--- pages/signin/index.js
+++ pages/signin/index.js
@@ -1,10 +1,30 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
+import { useFormik, Form, FormikProvider } from 'formik'
+import * as Yup from 'yup'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
 
-
 export default function index() {
-  
+  const LoginSchema = Yup.object().shape({
+    email: Yup.string()
+      .email('Email must be a valid email address')
+      .required('Email is required'),
+    password: Yup.string().required('Password is required'),
+  })
+
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validationSchema: LoginSchema,
+    onSubmit: () => {
+      console.log(values)
+    },
+  })
+
+  const { errors, setErrors, touched, values, handleSubmit, getFieldProps } =
+    formik
 
   return (
     <>
@@ -18,41 +38,72 @@ export default function index() {
             <div className="card shadow-lg  w-full h-full rounded-3xl absolute  transform -rotate-9"></div>
             <div className="card shadow-lg  w-full h-full rounded-3xl absolute  transform rotate-9"></div>
             <div className="relative w-full rounded-3xl  px-6 py-4 bg-gray-100 shadow-md">
-              <label
-                htmlFor=""
-                className="block mt-3 text-m text-gray-900 text-center font-semibold"
-              >
-              </label>
-              
-                <form method="#" action="#" className="mt-10">
-                  <div className='mt-4'>
-                    <label for="price" class="block text-sm font-medium text-gray-700">Email Address</label>
-                    <div class="mt-1 relative rounded-md shadow-sm">
-                      <input type="text" name="email" class="focus:ring-blue-500 focus:border-blue-500 block w-full p-3 sm:text-sm border-gray-300 rounded-md" placeholder="Enter Email"/>
-                    </div>
-                    {/* <strong class="text-red-500 text-xs">Email field is required</strong> */}
-                  </div>
-                  <div className='mt-4'>
-                    <label for="price" class="block text-sm font-medium text-gray-700">Password</label>
-                    <div class="mt-1 relative rounded-md shadow-sm">
-                      <input type="password" name="password" class="focus:ring-blue-500 focus:border-blue-500 block w-full p-3 sm:text-sm border-gray-300 rounded-md" placeholder="Enter Strong Password"/>
-                    </div>
-                    {/* <strong class="text-red-500 text-xs">Password field is required</strong> */}
-                  </div>
-                  <br/>
-                  <button
-                      className={`bg-blue-500 w-full py-3 rounded-xl text-white shadow-xl hover:shadow-inner focus:outline-none transition duration-500 ease-in-out  transform hover:-translate-x hover:scale-105 `}
+              <FormikProvider value={formik}>
+                <Form className="mt-10" noValidate onSubmit={handleSubmit}>
+                  <div className="mt-4">
+                    <label
+                      for="price"
+                      class="block text-sm font-medium text-gray-700"
                     >
-                      Sign In
-                    </button>       
-         <br/>
-         <br/>
-        <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}> Not registered yet? Register&nbsp;<a href= "#" > here </a> </div>
-    </form>
-    </div>
-  </div>
-   </div>
-    </div>
+                      Email Address
+                    </label>
+                    <div class="mt-1 relative rounded-md shadow-sm">
+                      <input
+                        type="text"
+                        name="email"
+                        class="focus:ring-blue-500 focus:border-blue-500 block w-full p-3 sm:text-sm border-gray-300 rounded-md"
+                        placeholder="Enter Email"
+                        {...getFieldProps('email')}
+                      />
+                    </div>
+                    <strong class="text-red-500 text-xs">
+                      {touched.email && errors.email}
+                    </strong>
+                  </div>
+                  <div className="mt-4">
+                    <label
+                      for="price"
+                      class="block text-sm font-medium text-gray-700"
+                    >
+                      Password
+                    </label>
+                    <div class="mt-1 relative rounded-md shadow-sm">
+                      <input
+                        type="password"
+                        name="password"
+                        class="focus:ring-blue-500 focus:border-blue-500 block w-full p-3 sm:text-sm border-gray-300 rounded-md"
+                        placeholder="Enter Strong Password"
+                        {...getFieldProps('password')}
+                      />
+                    </div>
+                    <strong class="text-red-500 text-xs">
+                      {touched.password && errors.password}
+                    </strong>
+                  </div>
+                  <br />
+                  <button
+                    className={`bg-blue-500 w-full py-3 rounded-xl text-white shadow-xl hover:shadow-inner focus:outline-none transition duration-500 ease-in-out  transform hover:-translate-x hover:scale-105 `}
+                    type="submit"
+                  >
+                    Sign In
+                  </button>
+                  <br />
+                  <br />
+                  <div
+                    style={{
+                      display: 'flex',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                    }}
+                  >
+                    Not registered yet? Register&nbsp;<a href="#"> here </a>{' '}
+                  </div>
+                </Form>
+              </FormikProvider>
+            </div>
+          </div>
+        </div>
+      </div>
       <Footer />
     </>
   )
